5bc1fe4b3d9d2024b29a76f7390e6800aa7ca5ca
parag <paragpatil187@gmail.com>
2022-07-27 14:30:47 +0530

    setup firebase account ans config basic setup for app

---
+++ src/Components/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBHsBaaKw02q1Lmbt_LQ7tYGXG6jApE1VQ",
+    authDomain: "chatapp-d00a9.firebaseapp.com",
+    projectId: "chatapp-d00a9",
+    storageBucket: "chatapp-d00a9.appspot.com",
+    messagingSenderId: "308937107084",
+    appId: "1:308937107084:web:c3fa0283fac79de06ebda9",
+    measurementId: "G-8L5LL2QDH6"
+  };
