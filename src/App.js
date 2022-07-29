2d327ea86e3b365d846f6908ce3dfdab6d01523f
parag <paragpatil187@gmail.com>
2022-07-29 17:58:16 +0530

    google auth from firebase for login component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import { useState } from 'react';
 import { Route,Router } from 'react-router-dom';
 import './App.css';
 import Chat from './Components/Chat';
+import Login from './Components/Login';
 import Sidebar from './Components/Sidebar';
 
 
 
 
 function App() {
-  const [user,setUser]=useState("parag")
+  const [user,setUser]=useState(null)
   return (
     <div className="App">
       {!user?(
-        <h1>login</h1>
+        <Login/>
       ):(
         <div className='app_body'>
         
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { auth, provider } from './firebaseconfig'
 import "./login.css"
+import Button from '@material-ui/core/Button'
 const Login = () => {
+    const signIn=()=>{
+        auth.signInWithPopup(provider).then(result=>
+            console.log(result)
+            ).catch(error=>(error.message))
+    }
   return (
     <div className='login'>
         <div className="login_container">
@@ -9,6 +16,7 @@ const Login = () => {
         <div className="login_text">
             <h1>sign in to App</h1>
         </div>
+        <Button onClick={signIn}>Sign In With Google</Button>
 
     </div>
   )
