6d51d46c42f2d0c83c27e5eeb4d1a5c43d033b8f
parag <paragpatil187@gmail.com>
2022-07-29 19:52:14 +0530

    error fix can see backend mesage and last seen with user name

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,94 +1,97 @@
 import React, { useEffect, useState } from 'react'
 import "./chat.css"
-import {useParams} from "react-router-dom"
-import { Avatar,IconButton } from '@material-ui/core'
-import  MoreVertIcon  from '@material-ui/icons/MoreVert'
+import { useParams } from "react-router-dom"
+import { Avatar, IconButton } from '@material-ui/core'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
 // import ChatIcon from "@material-ui/icons/Chat";
-import  InsertEmoticonIcon  from '@material-ui/icons/InsertEmoticon'
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon'
 import { MessageSharp, SearchOutlined } from '@material-ui/icons';
-import { AttachFile} from '@material-ui/icons';
+import { AttachFile } from '@material-ui/icons';
 import MicIcon from "@material-ui/icons/Mic"
 import db from './firebaseconfig';
 import { useStateValue } from './StateProvider'
 import firebase from 'firebase/compat/app';
 
 const Chat = () => {
-    const [avtarv,setavtarv]=useState("123");
-    const [input,setInput]=useState("");
-    const {roomId}=useParams();
-    const[roomName,setRoomName]=useState("");
-    const [{user},dispatch]=useStateValue();
-    const[messages,setMessages]=useState([])
-    useEffect(()=>{
-        if(roomId){
-            db.collection("room").doc(roomId).onSnapshot(snapshot =>(
+    const [avtarv, setavtarv] = useState("123");
+    const [input, setInput] = useState("");
+    const { roomId } = useParams();
+    const [roomName, setRoomName] = useState("");
+    const [{ user }, dispatch] = useStateValue();
+    const [messages, setMessages] = useState([])
+    useEffect(() => {
+        if (roomId) {
+            db.collection("rooms").doc(roomId).onSnapshot(snapshot => (
                 setRoomName(snapshot.data().name)
-            ))
-            db.collection("messages").orderBy("timestamp","asc").onSnapshot(snapshot=>(
-                setMessages(snapshot.docs.map((doc)=>
-                doc.data()))
-            ))
+            ));
+            db.collection("rooms")
+            .doc(roomId)
+            .collection("messages")
+            .orderBy("timestamp", "asc")
+            .onSnapshot((snapshot) => 
+                    setMessages(snapshot.docs.map((doc) =>
+                        doc.data()))
+                )
         }
 
-    },[roomId])
+    }, [roomId])
     useEffect(() => {
-      setavtarv(Math.floor(Math.random()*5000))
-    
-      
-    }, [])
-    const sendMessage=(e)=>{
+        setavtarv(Math.floor(Math.random() * 5000))
+
+
+    }, [roomId]);
+    const sendMessage = (e) => {
         e.preventDefault();
         db.collection("rooms").doc(roomId).collection("messages").add({
-           message:input,
-           name:user.displayName,
-           timestamp:firebase.firestore.FieldValue.serverTimestamp(),
-                })
+            message: input,
+            name: user.displayName,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        })
 
         setInput("")
 
     }
-  return (
-    <div className='chat'>
-        <div className="chat_header">
-        <Avatar src={`https://avatars.dicebear.com/api/human/${avtarv}.svg`} />
-        <div className="chat_header_info">
-            <h3>{roomName}</h3>
-            <p>Last seen{" "}{
-                new Date(messages[messages.length-1]?.timestamp?.toDate()).toUTCString()
-}</p>
-        </div>
-        <div className="chat_header_right">
-            <IconButton>
-                <SearchOutlined/>
-                
-            </IconButton>
-            <IconButton><AttachFile/></IconButton>
-            <IconButton> <MoreVertIcon/></IconButton>
-        </div>
-        </div>
-        <div className="chat_body">
-            {messages.map((e)=>(
+    return (
+        <div className='chat'>
+            <div className="chat_header">
+                <Avatar src={`https://avatars.dicebear.com/api/human/${avtarv}.svg`} />
+                <div className="chat_header_info">
+                    <h3>{roomName}</h3>
+                    <p>Last seen{" "}{
+                        new Date(messages[messages.length - 1]?.timestamp?.toDate()).toUTCString()
+                    }</p>
+                </div>
+                <div className="chat_header_right">
+                    <IconButton>
+                        <SearchOutlined />
 
-<p className={`chat_message ${true && "chat_reciever"}`}>
-<span className='chat_name'>{e.name}</span>
-{e.message}
-<span className="chat_timestamp">{
-    new Date(e.timestamp?.toDate().toUTCString())
-}</span>
-</p>
-            ))}
-            
+                    </IconButton>
+                    <IconButton><AttachFile /></IconButton>
+                    <IconButton> <MoreVertIcon /></IconButton>
+                </div>
+            </div>
+            <div className="chat_body">
+                {messages.map((e) => (
+
+                    <p className={`chat_message ${true && "chat_reciever"}`}>
+                        <span className='chat_name'>{e.name}</span>
+                        {e.message}
+                        <span className="chat_timestamp">{
+                            new Date(e.timestamp?.toDate()).toUTCString()}</span>
+                    </p>
+                ))}
+
+            </div>
+            <div className="chat_footer">
+                <InsertEmoticonIcon />
+                <form>
+                    <input type="text" placeholder='Type a message' value={input} onChange={(e) => e.target.value} />
+                    <button onClick={sendMessage}>send a message</button>
+                </form>
+                <MicIcon />
+            </div>
         </div>
-        <div className="chat_footer">
-            <InsertEmoticonIcon/>
-            <form>
-                <input type="text" placeholder='Type a message' value={input} onChange={(e)=>e.target.value}/>
-                <button onClick={sendMessage}>send a message</button>
-            </form>
-            <MicIcon/>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Chat
