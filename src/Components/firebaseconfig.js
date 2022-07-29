0687fd64b6fe446ecd5f63dbe9e4481bbd8df933
parag <paragpatil187@gmail.com>
2022-07-29 17:36:20 +0530

    error fixed

--- src/Components/firebaseconfig.js
+++ src/Components/firebaseconfig.js
@@ -1,4 +1,6 @@
-import firebase from "firebase";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 const firebaseConfig = {
     apiKey: "AIzaSyBHsBaaKw02q1Lmbt_LQ7tYGXG6jApE1VQ",
     authDomain: "chatapp-d00a9.firebaseapp.com",
